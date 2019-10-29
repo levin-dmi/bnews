b58db70053d59183f78471c8afdd07fa970f3fa6
Dmitry Levin <levin.dmi@gmail.com>
2019-10-29 22:39:06 +0300

    Now link from news point to news

--- bnews.py
+++ bnews.py
@@ -10,51 +10,60 @@ PAGE_LIST = {'Bridgesport': {'url': 'http://bridgesport.ru',
                              'encoding': 'cp1251',
                              'soup string':
                                  'table[class = "contentpaneopen"] td[valign = "top"][class != "createdate"]',
-                             'sign': '[bridgesport.ru](http://bridgesport.ru)', },
+                             'sign': 'bridgesport.ru', },
              'ebl': {'url': 'http://www.eurobridge.org',
                      'encoding': 'utf-8',
                      'soup string': 'div[class = "item-details"] h3',
-                     'sign': '[eurobridge.org](http://www.eurobridge.org)', },
+                     'link soup string': 'div[class = "item-details"] h3 > a',
+                     'sign': 'eurobridge.org', },
              'wbf': {'url': 'http://www.worldbridge.org',
                      'encoding': 'utf-8',
                      'soup string': 'div[class *= "WBF-News"] h2',
-                     'sign': '[worldbridge.org](http://www.worldbridge.org)', },
+                     'link soup string': 'div[class *= "WBF-News"] h2 > a',
+                     'sign': 'worldbridge.org', },
              'AKeluin': {'url': 'http://akelyin.ru',
-                                'encoding': 'utf-8',
-                                'soup string': 'h2',
-                                'sign': '[akelyin.ru](http://akelyin.ru)', },
+                         'encoding': 'utf-8',
+                         'soup string': 'h2',
+                         'sign': 'akelyin.ru', },
              'Bridgeclub-forum': {'url': 'http://www.bridgeclub.ru/forums/index.php?showforum=16',
                                   'encoding': 'cp1251',
                                   'soup string': 'span[id^="tid-span-"]',
-                                  'sign': '[bridgeclub.ru: Форум] \
-                                         (http://www.bridgeclub.ru/forums/index.php?showforum=16)', },
+                                  'link soup string': 'span[id^="tid-span-"] > a',
+                                  'sign': 'bridgeclub.ru: форум', },
              'Bridgeclub': {'url': 'http://www.bridgeclub.ru',
                             'encoding': 'cp1251',
                             'soup string':
                                 'table[width != "100%"] > tr > td[valign != "bottom"]',
-                            'sign': '[bridgeclub.ru](http://http://www.bridgeclub.ru)', },
+                            'link soup string': 'table[width != "100%"] > tr > td[valign != "bottom"] > a',
+                            'sign': 'bridgeclub.ru', },
              'Gambler-main': {'url': 'https://www.gambler.ru/forum/index.php?showforum=4',
                               'encoding': 'cp1251',
                               'soup string':
                                   'td > a[href*="showtopic"], td > b > a[href*="showtopic"]',
-                              'sign': '[gambler.ru: Бридж](https://www.gambler.ru/forum/index.php?showforum=4)', },
+                              'link soup string':
+                                  'td > a[href*="showtopic"], td > b > a[href*="showtopic"]',
+                              'sign': 'gambler.ru: бридж', },
              'Gambler-contest': {'url': 'https://www.gambler.ru/forum/index.php?showforum=24',
                                  'encoding': 'cp1251',
                                  'soup string':
                                      'td > a[href*="showtopic"], td > b > a[href*="showtopic"]',
-                                 'sign':
-                                     '[gambler.ru: Турниры](https://www.gambler.ru/forum/index.php?showforum=24)', },
+                                 'link soup string':
+                                     'td > a[href*="showtopic"], td > b > a[href*="showtopic"]',
+                                 'sign': 'gambler.ru: турниры', },
              'Gambler-referee': {'url': 'https://www.gambler.ru/forum/index.php?showforum=26',
                                  'encoding': 'cp1251',
                                  'soup string':
                                      'td > a[href*="showtopic"], td > b > a[href*="showtopic"]',
-                                 'sign':
-                                     '[gambler.ru: Cудейство](https://www.gambler.ru/forum/index.php?showforum=82)', },
+                                 'link soup string':
+                                     'td > a[href*="showtopic"], td > b > a[href*="showtopic"]',
+                                 'sign': 'gambler.ru: судейство', },
              'Gambler-hupp': {'url': 'https://www.gambler.ru/forum/index.php?showforum=82',
                               'encoding': 'cp1251',
                               'soup string':
                                   'td > a[href*="showtopic"], td > b > a[href*="showtopic"]',
-                              'sign': '[gambler.ru: Хюпп](https://www.gambler.ru/forum/index.php?showforum=82)', },
+                              'link soup string':
+                                  'td > a[href*="showtopic"], td > b > a[href*="showtopic"]',
+                              'sign': 'gambler.ru: хюпп', },
              }
 
 DEBUG_LEVEL = logging.DEBUG
@@ -77,8 +86,11 @@ def load_page(options: dict) -> str:
     return page.text
 
 
-def parsing_page(page: str, options: dict) -> list:
-    """Parsing html page and return list of news"""
+def parsing_page(page: str, soup_string: str, tag: str = "text") -> list:
+    """Parsing html page and return list
+        if tag not set then return text
+        else return tag
+    """
 
     # Get news
     soup = bs4.BeautifulSoup(page, 'html.parser')
@@ -87,12 +99,15 @@ def parsing_page(page: str, options: dict) -> list:
     for item in to_extract:
         item.extract()
 
-    tag_news = soup.select(options['soup string'])
+    tag_news = soup.select(soup_string)
 
     # Convert news (Tag) to list of strings
     str_news = []
     for t in tag_news:
-        st = t.get_text()
+        if tag == "text":
+            st = t.get_text()
+        else:
+            st = t[tag]
         st = st.replace('\r', '').replace('\n\n', '\n').strip()
         str_news.append(st) if st != '' else None
     return str_news
@@ -130,13 +145,26 @@ for current_page in PAGE_LIST:
         logging.warning('Can not read page {}: {}'.format(current_page, err))
         continue
 
-    current_news = parsing_page(loaded_page, PAGE_LIST[current_page])
+    current_news = []
+    nn = parsing_page(loaded_page, PAGE_LIST[current_page]['soup string'])  # list of news
+    if 'link soup string' in PAGE_LIST[current_page]:
+        ln = parsing_page(loaded_page, PAGE_LIST[current_page]['link soup string'], "href")  # list of links
+    else:
+        ln = [PAGE_LIST[current_page]['url']] * len(nn)
+    for (n, li) in zip(nn, ln):
+        if li[0:4] != 'http':
+            li = PAGE_LIST[current_page]['url'] + '/' + li  # correct relative link
+        new_el = {'news': n,
+                  'sign': PAGE_LIST[current_page]['sign'],
+                  'link': li, }
+        current_news.append(new_el)
 
     # Compare news and old_news
     if current_page in news_records:
-        for l in current_news:
-            if l not in news_records[current_page]:
-                l_with_sign = l + '\n' + PAGE_LIST[current_page]['sign']
+        for li in current_news:
+            if not any(nr['news'] == li['news'] for nr in news_records[current_page]):  # if new news not in records
+#            if li['news'] not in news_records[current_page]:
+                l_with_sign = li['news'] + '\n' + '[' + li['sign'] + '](' + li['link'] + ')'
                 if DEBUG_LEVEL == logging.DEBUG:
                     logging.debug('Debug channel message: {}'.format(l_with_sign))
                 else:
@@ -159,7 +187,8 @@ except Exception as err:
 
 logging.info('Bridge News Bot work done')
 
+
+
 # TODO Сделать чтобы не править в продакшене руками уровень логгирования и бот айди
 # TODO Сделать закрепленное сообщение в канале чтобы было видно что бот работает и что он делает
-# TODO Сделать переход с сообщения Телеграм сразу внутрь новости
-# TODO Ьолее длительное храниение новостей, чтобы исключить сбои
+# TODO Более длительное храниение новостей, чтобы исключить сбои
