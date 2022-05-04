6582725e54d8899901a0089a20a3049ea2e17e63
levin-dmi <levin.dmi@gmail.com>
2022-05-04 22:23:49 +0300

    Add config.yaml

--- bnews.py
+++ bnews.py
@@ -4,91 +4,15 @@ import pickle
 import telebot
 import logging
 import os
-
-CHANNEL_NAME = '@RuBridgeNews'
-PAGE_LIST = {'Bridgesport': {'url': 'http://bridgesport.ru',
-                             'encoding': 'utf-8',
-                             'soup string':
-                                 'section[class *= "CardSlider"] span[class = "CardSlider__ItemTitle"]',
-                             'link soup string': 'section[class *= "CardSlider"] a[class="CardSlider__ItemContent"]',
-                             'sign': 'bridgesport.ru', },
-             'Dzen-zbb': {'url': 'https://zen.yandex.ru/id/5bfe862c042fd800aa67035b',
-                          'encoding': 'utf-8',
-                          'soup string': 'div[class *= "card-title"] > div[class="clamp__text-expand"]',
-                          'link soup string': 'a[class="card-image-view__clickable"]',
-                          'max records': 3,
-                          'cat link after': '?',
-                          'sign': 'Записки бриджевой блондинки', },
-             'ebl': {'url': 'http://www.eurobridge.org',
-                     'encoding': 'utf-8',
-                     'soup string': 'div[class = "item-details"] h3',
-                     'link soup string': 'div[class = "item-details"] h3 > a',
-                     'sign': 'eurobridge.org', },
-             'wbf': {'url': 'http://www.worldbridge.org',
-                     'encoding': 'utf-8',
-                     'soup string': 'div[class *= "WBF-News"] h2',
-                     'link soup string': 'div[class *= "WBF-News"] h2 > a',
-                     'sign': 'worldbridge.org', },
-             'AKeluin': {'url': 'http://akelyin.ru',
-                         'encoding': 'utf-8',
-                         'soup string': 'h2',
-                         'sign': 'akelyin.ru', },
-             'Bridgeclub-forum': {'url': 'http://www.bridgeclub.ru/forums/index.php?showforum=16',
-                                  'encoding': 'cp1251',
-                                  'soup string': 'span[id^="tid-span-"]',
-                                  'link soup string': 'span[id^="tid-span-"] > a',
-                                  'sign': 'bridgeclub.ru: форум', },
-             'Bridgeclub': {'url': 'http://www.bridgeclub.ru',
-                            'encoding': 'cp1251',
-                            'soup string':
-                                'table[width != "100%"] > tr > td[valign != "bottom"]',
-                            'link soup string': 'table[width != "100%"] > tr > td[valign != "bottom"] > a',
-                            'sign': 'bridgeclub.ru', },
-             'Gambler-main': {'url': 'https://www.gambler.ru/forum/index.php?showforum=4',
-                              'encoding': 'cp1251',
-                              'soup string':
-                                  'td > a[href*="showtopic"], td > b > a[href*="showtopic"]',
-                              'link soup string':
-                                  'td > a[href*="showtopic"], td > b > a[href*="showtopic"]',
-                              'sign': 'gambler.ru: бридж', },
-             'Gambler-contest': {'url': 'https://www.gambler.ru/forum/index.php?showforum=24',
-                                 'encoding': 'cp1251',
-                                 'soup string':
-                                     'td > a[href*="showtopic"], td > b > a[href*="showtopic"]',
-                                 'link soup string':
-                                     'td > a[href*="showtopic"], td > b > a[href*="showtopic"]',
-                                 'sign': 'gambler.ru: турниры', },
-             'Gambler-referee': {'url': 'https://www.gambler.ru/forum/index.php?showforum=26',
-                                 'encoding': 'cp1251',
-                                 'soup string':
-                                     'td > a[href*="showtopic"], td > b > a[href*="showtopic"]',
-                                 'link soup string':
-                                     'td > a[href*="showtopic"], td > b > a[href*="showtopic"]',
-                                 'sign': 'gambler.ru: судейство', },
-             'Gambler-hupp': {'url': 'https://www.gambler.ru/forum/index.php?showforum=82',
-                              'encoding': 'cp1251',
-                              'soup string':
-                                  'td > a[href*="showtopic"], td > b > a[href*="showtopic"]',
-                              'link soup string':
-                                  'td > a[href*="showtopic"], td > b > a[href*="showtopic"]',
-                              'sign': 'gambler.ru: хюпп', },
-             }
-
-DEBUG_LEVEL = logging.DEBUG
-DATA_FILE_NAME = 'bnews.dat'
-LOG_FILE_NAME = 'bnews.log'
-TELEGRAM_BOT_ID = ''
-
-try:
-    from dev_settings import *
-except ImportError:
-    pass
+import yaml
 
 
 def load_page(options: dict) -> str:
     """Load page from site"""
-
-    page = requests.get(options['url'])
+    user_agent = ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko)'
+                 'Chrome/50.0.2661.102 Safari/537.36')
+    headers = {'User-Agent': user_agent}
+    page = requests.get(options['url'], headers=headers)
     page.raise_for_status()  # Do exception if code not 400 OK
     page.encoding = options['encoding'] if 'encoding' in options else None
     return page.text
@@ -121,19 +45,45 @@ def parsing_page(page: str, soup_string: str, tag: str = "text") -> list:
     return str_news
 
 
-# Set script_data and script_log file name
 script_dir = os.path.dirname(os.path.abspath(__file__))
-script_data_file = os.path.join(script_dir, DATA_FILE_NAME)
-script_log_file = os.path.join(script_dir, LOG_FILE_NAME)
+script_data_file = os.path.join(script_dir, 'bnews.dat')
+script_log_file = os.path.join(script_dir, 'bnews.log')
+script_config_file = os.path.join(script_dir, 'config.yaml')
 
+# Load configuration file
+with open(script_config_file, encoding='utf-8') as fh:
+    loaded_config = yaml.load(fh, Loader=yaml.FullLoader)
+
+# Set log level and start logging
+DEBUG_LEVEL = logging.DEBUG
+if 'log_level' in loaded_config:
+    if loaded_config['log_level'] == 'info':
+        DEBUG_LEVEL = logging.INFO
+    elif loaded_config['log_level'] == 'error':
+        DEBUG_LEVEL = logging.ERROR
 logging.basicConfig(format='%(asctime)s.%(msecs)d %(levelname)s in \'%(module)s\' at line %(lineno)d: %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S',
+                    datefmt='%d-%m-%Y %H:%M:%S',
                     level=DEBUG_LEVEL,
                     filename=script_log_file)
+logging.info('Bot started')
 
-logging.info('Bridge News Bot started')
+#  Set telegram parameters
+try:
+    CHANNEL_NAME = loaded_config['telegram']['channel_name']
+    TELEGRAM_BOT_ID = loaded_config['telegram']['telegram_bot_id']
+    TELEGRAM_ON = True
+except KeyError:
+    TELEGRAM_ON = False
 
-bot = telebot.TeleBot(TELEGRAM_BOT_ID)
+#  Load page list
+try:
+    PAGE_LIST = loaded_config['pages']
+except KeyError:
+    logging.error("Can't loading pages list from config file")
+    raise SystemExit(1)
+
+if TELEGRAM_ON:
+    bot = telebot.TeleBot(TELEGRAM_BOT_ID)
 
 # Try to load old news
 try:
@@ -177,7 +127,7 @@ for current_page in PAGE_LIST:
             else:
                 li = PAGE_LIST[current_page]['url'] + '/' + li
         new_el = {'news': n,
-                  'sign': PAGE_LIST[current_page]['sign'],
+                  'sign': current_page,
                   'link': li, }
         current_news.append(new_el)
 
@@ -186,8 +136,8 @@ for current_page in PAGE_LIST:
         for li in current_news:
             if not any(nr['news'] == li['news'] for nr in news_records[current_page]):  # if new news not in records
                 l_with_sign = li['news'] + '\n' + '[' + li['sign'] + '](' + li['link'] + ')'
-                if DEBUG_LEVEL == logging.DEBUG:
-                    logging.debug('Debug channel message: {}'.format(l_with_sign))
+                if not TELEGRAM_ON:
+                    logging.info('Debug channel message: {}'.format(l_with_sign))
                 else:
                     try:
                         bot.send_message(CHANNEL_NAME, l_with_sign, parse_mode='Markdown',
@@ -195,6 +145,10 @@ for current_page in PAGE_LIST:
                     except Exception as err:
                         logging.error('Error to send message to channel: {}'.format(str(err)))
 
+    else:
+        for li in current_news:
+            l_with_sign = li['news'] + '\n' + '[' + li['sign'] + '](' + li['link'] + ')'
+            logging.info('New chanel added with message: {}'.format(l_with_sign))
     # Save news
     news_records[current_page] = current_news
 
@@ -208,7 +162,7 @@ except Exception as err:
 
 logging.info('Bridge News Bot work done')
 
-
+# TODO Обновить requirements
 # TODO Разобраться с git clone на удаленном сервере, проверить тесты, улучшить читаемость переменные и добавить функций
 # TODO Сделать чтобы не править в продакшене руками уровень логгирования и бот айди
 # TODO Сделать закрепленное сообщение в канале чтобы было видно что бот работает и что он делает
